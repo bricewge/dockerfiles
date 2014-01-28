0a83572708ceaf4b54c5cf27157e728d208731c3
Brice Waegeneire <brice.wge@gmail.com>
2014-01-28 16:57:47 +0100

    rpi-flexget specify configuration file and use simple path for configuration file

--- rpi-flexget/flexget.sh
+++ rpi-flexget/flexget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Set transmission host based on environement variable provided by docker
-sed -i '/host:/c\      host: '$TRANS_PORT_9091_TCP_ADDR'' /home/flexget/.flexget/config.yml
+sed -i '/host:/c\      host: '$TRANS_PORT_9091_TCP_ADDR'' /home/flexget/config.yml
 
-/usr/local/bin/flexget daemon start
+/usr/local/bin/flexget -c /home/flexget/config.yml daemon start
