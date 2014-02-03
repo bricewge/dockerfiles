6c5509eb00f9ecc0e8e52c7fa99fe8ae8179ad95
Brice Waegeneire <brice.wge@gmail.com>
2014-02-03 23:58:07 +0100

    rpi-flexget now based on rpi-ssh, running supervisor

--- rpi-flexget/flexget.sh
+++ rpi-flexget/flexget.sh
@@ -3,4 +3,4 @@
 # Set transmission host based on environement variable provided by docker
 sed -i '/host:/c\      host: '$TRANS_PORT_9091_TCP_ADDR'' /home/flexget/config.yml
 
-/usr/local/bin/flexget -c /home/flexget/config.yml daemon start
+/usr/bin/sudo -u flexget /usr/local/bin/flexget -c /home/flexget/config.yml daemon start
