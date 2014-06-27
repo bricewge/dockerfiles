2779341e2b4059f13f346d86b2c2a5c1c6090d3c
Brice Waegeneire <brice.wge@gmail.com>
2014-06-27 23:21:59 +0200

    Add bricewge/transmission

---
+++ x86_64/transmission/10-transmission.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PASSWD_T=${PASSWD_T:-}
+USER_T=${USER_T:-}
+WHITELIST_T=${$WHITELIST_T:-}
+
+transmission_settings=/etc/transmission-daemon/settings.json
+
+sed -i 's:\"password\":\"'"$PASSWD_T"'\":g' $transmission_settings
+sed -i 's:\"transmission\":\"'"$USER_T"'\":g' $transmission_settings
+sed -i 's:\"127.0.0.1,192.168.1.*\":\"'"$WHITELIST_T"'\":g' $transmission_settings
