37409655f9c042d4888fdb0b6e75d09d42f1b4ef
Brice Waegeneire <brice.wge@gmail.com>
2014-08-16 21:02:52 +0200

    Add COPS receipe, first draft

---
+++ x86_64/cops/config_local.php
@@ -0,0 +1,17 @@
+if (!isset($config))
+        $config = array();
+
+    $config['calibre_directory'] = '/mnt/Livres';
+
+    $config['calibre_internal_directory'] = '/Calibre/'; 
+
+    $config['cops_full_url'] = 'biblio.bricewge.fr'; 
+
+    $config['cops_x_accel_redirect'] = "X-Accel-Redirect";
+
+    $config['cops_use_url_rewriting'] = "1";
+
+
+}
+
+
