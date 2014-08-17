3e384318af21b2910d0826ef1e67431a0d789aa6
bricewge <bricewge@durian.bricewge.fr>
2014-08-17 23:15:44 +0200

    Working version of x86_64/cops

--- x86_64/cops/config_local.php
+++ x86_64/cops/config_local.php
@@ -1,17 +1,31 @@
-if (!isset($config))
+<?php
+    if (!isset($config))
         $config = array();
+  
+    /*
+     * The directory containing calibre's metadata.db file, with sub-directories
+     * containing all the formats.
+     * BEWARE : it has to end with a /
+     */
+    $config['calibre_directory'] = '/mnt/Livres/';
+    
+    /*
+     * Catalog's title
+     */
+    $config['cops_title_default'] = "Bibliothèque";
+    
+    /*
+     * use URL rewriting for downloading of ebook in HTML catalog
+     * See README for more information
+     *  1 : enable
+     *  0 : disable
+     */
+    $config['cops_use_url_rewriting'] = "0";
 
-    $config['calibre_directory'] = '/mnt/Livres';
-
-    $config['calibre_internal_directory'] = '/Calibre/'; 
-
-    $config['cops_full_url'] = 'biblio.bricewge.fr'; 
-
-    $config['cops_x_accel_redirect'] = "X-Accel-Redirect";
-
-    $config['cops_use_url_rewriting'] = "1";
-
-
-}
-
-
+    /*
+     *$config['calibre_internal_directory'] = ''; 
+     *
+     *$config['cops_full_url'] = 'biblio.bricewge.fr'; 
+     *
+     *$config['cops_x_accel_redirect'] = "X-Accel-Redirect";
+     */
